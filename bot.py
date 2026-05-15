0aeb8ff61cdfb8303e8f2d70b2483b0ea01c2839
awseedrf1 <awseedrf_sv@hotmail.com>
2026-05-16 01:15:47 +0700

    Add files via upload

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.voice_states = True
 
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 # อ่าน Channel ID ที่อนุญาตให้ใช้คำสั่ง (รองรับหลาย channel คั่นด้วย ,)
 ALLOWED_CHANNEL_IDS = set()
