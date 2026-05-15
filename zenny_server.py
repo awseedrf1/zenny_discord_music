61805c81b721645f1f544193003e8cda8b0c5993
awseedrf1 <awseedrf_sv@hotmail.com>
2026-05-15 22:08:33 +0700

    Update home route response and rename server function

--- zenny_server.py
+++ zenny_server.py
@@ -5,11 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Server is running!"
+    return "Zenny BOT!, Gu Alive UUU!!!"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
 
-def server_on():
+def keep_alive():
     t = Thread(target=run)
     t.start()
